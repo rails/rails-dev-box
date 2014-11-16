dbd73129674cc261e47fd9b391c51fa6f3f59dbd
Rainer Borene <rainerborene@gmail.com>
2014-11-16 17:57:28 -0200

    Install apt-add-repository command.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
+install 'apt-add-repository' software-properties-common python-software-properties
+
 echo updating package information
 apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
