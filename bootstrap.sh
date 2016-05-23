586539c1e2d665a8e9e3f00b8b0c9bb04526e6a6
Mehmet Emin İNAÇ <mehmetemininac@gmail.com>
2016-05-23 14:52:59 +0300

    Install ncurses dev dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,6 +52,8 @@ SQL
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
 install 'ExecJS runtime' nodejs
 
+install libncurses5-dev
+
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
