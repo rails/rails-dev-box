7cb1f29fcb21bd54460385c890097e38f28d52fb
Xavier Noria <fxn@hashref.com>
2018-05-05 19:13:09 +0200

    install autoconf and libtool
    These are necessary packages to be able to compile ffi.

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ echo updating package information
 apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
-install 'development tools' build-essential
+install 'development tools' build-essential autoconf libtool
 
 install Ruby ruby2.5 ruby2.5-dev
 update-alternatives --set ruby /usr/bin/ruby2.5 >/dev/null 2>&1
