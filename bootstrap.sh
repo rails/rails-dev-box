6f43d75f85dcf55468dc6592e9f7fb2f728b1222
Nixon <lazaronixon@hotmail.com>
2023-05-15 15:32:29 -0300

    add missing dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,6 +72,9 @@ install 'MuPDF' mupdf mupdf-tools
 install 'FFmpeg' ffmpeg
 install 'Poppler' poppler-utils
 
+install 'psych dependencies' libyaml-dev
+install 'readline-ext dependencies' libreadline-dev
+
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
